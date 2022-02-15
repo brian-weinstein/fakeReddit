01537e2f3a2ef1a66dfc91629c3cb2ee316c1ba7
Brian <brianmweinstein@gmail.com>
2022-02-15 13:59:40 -0500

    Sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,21 @@
-def new
-    render :new
-end
-
-def create
-    @user = User.find_by_credentials(params[:user][:username],params[:user][:password])
-    if @user
-        login_user!(user)
-        redirect_to root_url
-    else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+class SessionsController < ApplicationController
+    def new
+        render :new
     end
-end
 
-def destroy
+    def create
+        @user = User.find_by_credentials(params[:user][:username],params[:user][:password])
+        if @user
+            login_user!(user)
+            redirect_to root_url
+        else
+            format.html { render :new, status: :unprocessable_entity }
+            format.json { render json: @user.errors, status: :unprocessable_entity }
+        end
+    end
 
+    def destroy
+        logout_user!
+        redirect_to new_session_url
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
     respond_to do |format|
       if @user.save
         login_user!(@user)
-        format.html { redirect_to user_url(@user), notice: "User was successfully created." }
+        format.html { redirect_to root_url, notice: "User was successfully created." }
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new, status: :unprocessable_entity }
